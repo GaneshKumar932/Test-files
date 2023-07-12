1302f54a001deb6fbc7e704495bdc4d33671876a
Ganesh Kumar <gk@9989915@gmail.com>
2023-07-12 19:38:59 +0530

    changed some files and its data

---
+++ test.js
@@ -0,0 +1,43 @@
+function compressString(str) {
+    let compressed = '';
+    let count = 1;
+  
+    for (let i = 1; i <= str.length; i++) {
+      if (str[i] === str[i - 1]) {
+        count++;
+      } else {
+        compressed += count + str[i - 1];
+        count = 1;
+      }
+    }
+  
+    return compressed;
+  }
+  
+  const input = 'AAAABBBCCDAA';
+  const compressedString = compressString(input);
+  console.log(compressedString);
+  document.getElementById('test').innerHTML = compressedString ;
+
+
+function findrepeat(array){
+  let count = [] ;
+  for (let i = 0; i<= Math.max(...array) ; i++) {
+    let filtered = array.filter((val)=>{return val===i;});
+    if(filtered.length > 0){
+      count[i]= filtered.length
+    }else{
+      count[i]=0;
+    }
+  }
+  console.log(count);
+  if(count.length > 0){
+    return Math.max(...count)* count.lastIndexOf(Math.max(...count))
+  }
+};
+    
+const array1= [1,1,2,2,2,2,2,150,150,150,150,150,150,4,4,4,4];
+const finalresult = findrepeat(array1);
+console.log(finalresult);
+console.log('hi');
+
